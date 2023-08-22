e128963bde5f7c250aa6563923c6f52cc5106bb1
374688995@qq.com <374688995@qq.com>
2023-08-22 20:39:26 +0800

    创建变量文件

--- Project1/hello world.cpp
+++ Project1/hello world.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+int main1() {
+	// 输出内容
 	cout << "hello world" << endl;
+	// 下面两行是每个程序中都有的两行
 	system("pause");
 	return 0;
 }
---
+++ Project1/variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	/* 变量
+	* 作用：给一段指定的内存空间起名，方便操作这段内存
+	* 语法：变量类型 变量名=初始值
+	*/
+	int a = 10;
+	cout << "a = " << a << endl;
+	system("pause");
+	return 0;
+}
