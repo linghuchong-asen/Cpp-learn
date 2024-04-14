fe337810f388d194177fe26f51b57ca33a6ac1bf
374688995 <374688995@qq.com>
2024-04-14 21:57:01 +0800

    家里电脑

--- Project1/helloWorld.h
+++ Project1/helloWorld.h
@@ -4,9 +4,8 @@
 
 /** ¥Ú”°hello world */
 int hello();
-<<<<<<< HEAD
+
 void swapFun();
-=======
->>>>>>> b1045c35cb84c8bd8c14868adb19e5d47e71dd11
+
 
 #endif // !hello world
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,14 +5,17 @@ using namespace std;
 extern int hello(); // 表明这个这个函数式外部引用的
 extern int variable();
 
+
 int main() {
-<<<<<<< HEAD
+
 	//hello();
 	//variable();
 	swapFun();
-=======
+
 	hello();
 	variable();
->>>>>>> b1045c35cb84c8bd8c14868adb19e5d47e71dd11
+
 	return 0;
+	
 }
+
