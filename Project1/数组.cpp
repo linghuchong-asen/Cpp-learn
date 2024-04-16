e149755fb0ecc3985cb476f42a4c4a2e2694f472
374688995@qq.com <374688995@qq.com>
2024-04-16 18:08:53 +0800

    数组

--- Project1/数组.cpp
+++ Project1/数组.cpp
@@ -3,7 +3,7 @@
 void arrFun() {
 	int arr[5] = { 0,1,2,3,4 };
 
-	//arr和&arr一直都是数组的首地址，即一个元素的地址
+	//arr和&arr一致都是数组的首地址，即一个元素的地址；这一点和js不一样，js打印arr就是这个数组本身了
 	cout << "arr的值" << arr << endl;
 	cout << "arr的地址" << &arr << endl;
 
